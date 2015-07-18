7ffd5dcde1e81ad774ee2df600f261e3ede80be2
conglomerator <>
2015-07-18 14:35:51 -0400

    Refined GUI template, built "Input Binding" test component

--- js/jwl.js
+++ js/jwl.js
@@ -1,17 +1,18 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-var displayMessage = function(data){
-    if (data.message) $(".jwl-alert-container").append("<div class='alert jwl-alert alert-"+data.message.type+" alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"+data.message.text+"</div>");
+var displayAlert = function (alert) {
+    if (!alert.type || (alert.type != "success" && alert.type != "info" && alert.type != "danger" && alert.type != "warning")) alert.type = "info";
+    if (!alert.text||alert.text=="") switch (alert.type) {
+            case "success":
+                alert.text = "Success!";
+                break;
+            case "info":
+                alert.text = "Please take note.";
+                break;
+            case "warning":
+                alert.text = "Warning!";
+                break;
+            case "danger":
+                alert.text = "Danger!";
+                break;
+    };
+    $(".jwl-alert-container").append("<div class='alert jwl-alert alert-"+alert.type+" alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"+alert.text+"</div>");
 };
-
-var test = function(){
-    $.getJSON("/php/index.php",displayMessage);
-};
-
-$(document).ready(function(){
-    $(".jwl-test-navitem").click(test);
-});
--- php/rand.php
+++ php/rand.php
@@ -1,5 +1,3 @@
 <?php
-$message = array('type'=>'info','text'=>'This is a test message.');
-$json = array('message'=>$message);
 header('Content-Type: application/json');
-echo(json_encode($json));
+echo(json_encode(rand(0,10000)));
