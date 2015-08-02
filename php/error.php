f83300574b384200c4d50da424d89fdfa2123822
conglomerator <>
2015-08-01 23:15:52 -0400

    More error handling changes

--- php/error.php
+++ php/error.php
@@ -1,5 +1,11 @@
 <?php
 
+// Load JWL library
+require_once("jwl.php");
+
+// Set PHP error handling options
+JWLsetErrorHandlingOptions();
+
 // Send some test errors
 error_log("This is a test error from error_log().");
 trigger_error("This is a test error from trigger_error().");
--- php/jwl.php
+++ php/jwl.php
@@ -1,5 +1,5 @@
 <?php
 
 function JWLsetErrorHandlingOptions() {
-    return (ini_set("display_errors",1)&&ini_set("error_reporting",E_ALL)&&ini_set("log_errors",1)&&ini_set("error_log","/Users/joe/NetBeansProjects/btbadmin/test.log"));
+    return (ini_set("display_errors",0)&&ini_set("error_reporting",E_ALL)&&ini_set("log_errors",1)&&ini_set("error_log","test.log"));
 }
