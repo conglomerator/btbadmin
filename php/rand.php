b810e62bc7771c6d0b4b03dde2a1782f92a676c6
conglomerator <>
2015-08-01 23:57:37 -0400

    More error handling changes, log message added to rand.php

--- php/rand.php
+++ php/rand.php
@@ -1,5 +1,16 @@
 <?php
 
+// Load JWL library
+require_once("jwl.php");
+
+// Set PHP error handling options
+JWLsetErrorHandlingOptions();
+
+// Log script usage
+$userMessage = "";
+if ($_SERVER["PHP_AUTH_USER"]) $userMessage = "by ".$_SERVER["PHP_AUTH_USER"];
+trigger_error("Random number script begun".$userMessage);
+
 // Send random number
 header('Content-Type: application/json');
 echo(json_encode(mt_rand(0,1000)));
