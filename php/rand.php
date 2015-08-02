11da749175be618e8fbbad99de9e7a6ace0d324e
conglomerator <>
2015-08-02 00:15:05 -0400

    More log messages added to rand.php

--- php/rand.php
+++ php/rand.php
@@ -6,11 +6,19 @@ require_once("jwl.php");
 // Set PHP error handling options
 JWLsetErrorHandlingOptions();
 
-// Log script usage
-$userMessage = "";
-if ($_SERVER["PHP_AUTH_USER"]) $userMessage = "by ".$_SERVER["PHP_AUTH_USER"];
-trigger_error("Random number script begun".$userMessage);
+// Log script start
+error_log("[".$_SERVER["PHP_AUTH_USER"]."] [".$_SERVER["REQUEST_URI"]."] [".$_SERVER["REQUEST_TIME"]."] Script started.");
+
+// Generate random number
+$randNum = mt_rand(0,10000);
 
 // Send random number
 header('Content-Type: application/json');
-echo(json_encode(mt_rand(0,1000)));
+echo(json_encode($randNum));
+
+// Log random number
+error_log("[".$_SERVER["PHP_AUTH_USER"]."] [".$_SERVER["REQUEST_URI"]."] [".$_SERVER["REQUEST_TIME"]."] Random number ".$randNum." sent.");
+
+
+// Log script end
+error_log("[".$_SERVER["PHP_AUTH_USER"]."] [".$_SERVER["REQUEST_URI"]."] [".$_SERVER["REQUEST_TIME"]."] Script ended.");
