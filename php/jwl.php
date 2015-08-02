401d45134b4093a9ae2cc00a82a56b3f331b4d76
conglomerator <>
2015-08-01 23:45:55 -0400

    More error handling changes

--- php/jwl.php
+++ php/jwl.php
@@ -1,5 +1,8 @@
 <?php
 
+define("JWL_LOGGING",0);
+define("JWL_LOG_TARGET",NULL);
+
 function JWLsetErrorHandlingOptions() {
-    return (ini_set("display_errors",0)&&ini_set("error_reporting",E_ALL)&&ini_set("log_errors",1)&&ini_set("error_log","test.log"));
+    return (ini_set("display_errors",0)&&ini_set("error_reporting",E_ALL)&&ini_set("log_errors",JWL_LOGGING)&&ini_set("error_log",JWL_LOG_TARGET));
 }
