d97b7967178755a8af1a77492cc45253f673eb86
conglomerator <>
2015-08-02 23:12:41 -0400

    Verified functional script

--- php/get-product-group.php
+++ php/get-product-group.php
@@ -1,19 +1,24 @@
 <?php
 
 // Load JWL PHP
-require_once("jwl.php");
+//require_once("jwl.php");
 
 define('MYSQL_USERNAME', 'cocheese_1911');
-define('MYSQL_PASSWORD', 'JxMa2e8'); 
+define('MYSQL_PASSWORD', 'JxMa2e8');
 define('MYSQL_HOSTNAME', 'localhost');
+define('MYSQL_PDGDBNAME','cocheese_pdgcommerce');
 
-$db_handle = new PDO('mysql:host='.MYSQL_HOSTNAME.';dbname=pdgcommerce',MYSQL_USERNAME,MYSQL_PASSWORD);
+$db_handle = new PDO('mysql:host='.MYSQL_HOSTNAME.';dbname='.MYSQL_PDGDBNAME,MYSQL_USERNAME,MYSQL_PASSWORD);
 
 $field = $_GET['field'];
 $value = $_GET['value'];
 
-if ($field&&$value) $query = $db_handle->query('SELECT * FROM PRODUCTS WHERE ' . mysql_escape_string($field) . ' LIKE "%' . mysql_escape_string($value) . '%" ORDER BY PR_SKU DESC');
+$resultSet = array();
+if ($field&&$value) {
+    $query = $db_handle->query('SELECT * FROM PRODUCTS WHERE ' . mysql_escape_string($field) . ' LIKE "%' . mysql_escape_string($value) . '%" ORDER BY PR_SKU DESC');
+    $resultSet = $query->fetchAll(PDO::FETCH_ASSOC);
+}
 
 // Send result set
 header('Content-Type: application/json');
-echo(json_encode($query->fetchAll(PDO::FETCH_ASSOC)));
+echo(json_encode($resultSet));
