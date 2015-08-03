e82d22023fc82abb7ffc331a6b97b72dd2a77f4c
conglomerator <>
2015-08-02 20:52:49 -0400

    More small changes

--- php/rand.php
+++ php/rand.php
@@ -1,13 +1,7 @@
 <?php
 
-// Load JWL library
+// Load JWL PHP
 require_once("jwl.php");
-
-// Set PHP error handling options
-JWLsetErrorHandlingOptions();
-
-// Log script start
-error_log("[".$_SERVER["PHP_AUTH_USER"]."] [".$_SERVER["REQUEST_URI"]."] [".$_SERVER["REQUEST_TIME"]."] Script started.");
 
 // Generate random number
 $randNum = mt_rand(0,100000);
@@ -15,10 +9,3 @@ $randNum = mt_rand(0,100000);
 // Send random number
 header('Content-Type: application/json');
 echo(json_encode($randNum));
-
-// Log random number
-error_log("[".$_SERVER["PHP_AUTH_USER"]."] [".$_SERVER["REQUEST_URI"]."] [".$_SERVER["REQUEST_TIME"]."] Random number ".$randNum." sent.");
-
-
-// Log script end
-error_log("[".$_SERVER["PHP_AUTH_USER"]."] [".$_SERVER["REQUEST_URI"]."] [".$_SERVER["REQUEST_TIME"]."] Script ended.");
