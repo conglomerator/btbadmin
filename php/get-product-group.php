1838f4461955305f61dc4683e2672f0e4556ef98
conglomerator <>
2015-08-02 22:45:35 -0400

    Began work on product group editor

---
+++ php/get-product-group.php
@@ -0,0 +1,19 @@
+<?php
+
+// Load JWL PHP
+require_once("jwl.php");
+
+define('MYSQL_USERNAME', 'cocheese_1911');
+define('MYSQL_PASSWORD', 'JxMa2e8'); 
+define('MYSQL_HOSTNAME', 'localhost');
+
+$db_handle = new PDO('mysql:host='.MYSQL_HOSTNAME.';dbname=pdgcommerce',MYSQL_USERNAME,MYSQL_PASSWORD);
+
+$field = $_GET['searchField'];
+$value = $_GET['searchValue'];
+
+$query = $db_handle->query('SELECT * FROM PRODUCTS WHERE ' . mysql_escape_string($field) . ' LIKE "%' . mysql_escape_string($value) . '%" ORDER BY PR_SKU DESC');
+
+// Send result set
+header('Content-Type: application/json');
+echo(json_encode($query->fetchAll(PDO::FETCH_ASSOC)));
--- php/jwl.php
+++ php/jwl.php
@@ -1,10 +1,1 @@
 <?php
-
-// Configuration constants with defaults set
-define("JWL_LOGGING",0);
-define("JWL_LOG_TARGET",NULL);
-
-// Function to set error handling (i.e. don't output any messages and log all messages to target defined in config constant)
-function JWLsetErrorHandlingOptions() {
-    return (ini_set("display_errors",0)&&ini_set("error_reporting",E_ALL)&&ini_set("log_errors",JWL_LOGGING)&&ini_set("error_log",JWL_LOG_TARGET));
-}
