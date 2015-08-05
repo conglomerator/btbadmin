35fc02f58ee33c5e2e78fb388af9b854049233b2
conglomerator <>
2015-08-04 21:50:18 -0400

    Updated config setup, worked on get group

---
+++ php/get-group.php
@@ -0,0 +1,29 @@
+<?php
+
+// Load JWL PHP
+require_once("jwl.php");
+
+$db_handle = new PDO('mysql:host='.$_JWL['DB_HOSTNAME'].';dbname='.$_JWL['DB_PDGDBNAME'],$_JWL['DB_USERNAME'],$_JWL['DB_PASSWORD']);
+
+$field = $_GET['field'];
+$value = $_GET['value'];
+
+// Execute query
+$resultSet = array();
+$columns = array();
+if ($field&&$value) {
+    $query = $db_handle->query('SELECT * FROM PRODUCTS WHERE ' . mysql_escape_string($field) . ' LIKE "%' . mysql_escape_string($value) . '%" ORDER BY PR_SKU DESC');
+    
+    // Derive columns
+    for ($i = 0; $i<$query->columnCount(); $i++) {
+        $columnMeta = $query->getColumnMeta($i);
+        array_push($columns, $columnMeta['name']);
+    }
+    
+    // Fetch results
+    $resultSet = $query->fetchAll(PDO::FETCH_ASSOC);
+}
+
+// Send result set
+header('Content-Type: application/json');
+echo(json_encode(array('columns'=>$columns,'results'=>$resultSet)));
--- php/jwl.php
+++ php/jwl.php
@@ -1,1 +1,10 @@
 <?php
+
+$_JWL = array(
+    'JWL_CONFIG_PATH'   => '',
+    'DB_HOSTNAME'       => '',
+    'DB_PDGDBNAME'      => '',
+    'DB_JWLDBNAME'      => '',
+    'DB_USERNAME'       => '',
+    'DB_PASSWORD'       => ''
+);
