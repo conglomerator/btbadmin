7f70d0478a1e9f1df667632e34569fdf04ddca72
conglomerator <>
2015-08-04 21:56:11 -0400

    Updated config setup

---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$_JWL = array(
+    'DB_HOSTNAME'       => '',
+    'DB_PDGDBNAME'      => '',
+    'DB_JWLDBNAME'      => '',
+    'DB_USERNAME'       => '',
+    'DB_PASSWORD'       => ''
+);
--- php/jwl.php
+++ php/jwl.php
@@ -1,10 +1,4 @@
 <?php
 
-$_JWL = array(
-    'JWL_CONFIG_PATH'   => '',
-    'DB_HOSTNAME'       => '',
-    'DB_PDGDBNAME'      => '',
-    'DB_JWLDBNAME'      => '',
-    'DB_USERNAME'       => '',
-    'DB_PASSWORD'       => ''
-);
+// Require config file; need to specify path
+require_once('');
