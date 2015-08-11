8b519a6b103d6032b047817f5024c703a00d8f92
conglomerator <>
2015-08-11 06:57:10 -0400

    Changed common PHP to get logging working

--- public/api/common.php
+++ public/api/common.php
@@ -5,6 +5,6 @@
 
 // Set logging parameters
 ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 0);
-ini_set('log_errors', 1);
+ini_set('display_errors', '0');
+ini_set('log_errors', true);
 ini_set('error_log', $_CONFIG['LOG_DESTINATION']);
